2ac7f5e7cbe49b355ede2bf6d36463c9b7ea66ce
Cheng <cheng.lu.fred@gmail.com>
2023-01-09 16:42:13 -0500

    replicate Rapach 2010 prediction

--- code/module/data_handler.py
+++ code/module/data_handler.py
@@ -1,12 +1,14 @@
 from __future__ import annotations
 import pandas as pd
 import numpy as np
+import re
 import matplotlib.pyplot as plt
 # matplotlib.use('Agg')
 import datetime
 import warnings
 import itertools
 warnings.filterwarnings("ignore")
+
 
 from finrl.meta.preprocessor.yahoodownloader import YahooDownloader
 from finrl.meta.preprocessor.preprocessors import FeatureEngineer, data_split
@@ -61,7 +63,7 @@ def feature_engineering(TRAIN_START_DATE:str = '2009-01-01',
     return train, test, info_dict
 
 
-def get_monthly_date_format(date_string:str) -> pd.Period:
+def get_monthly_date_format(date_string:str, kwargs: dict = {}) -> pd.Period:
     """
     Get the monthly date format for a given date string.
 
@@ -69,11 +71,102 @@ def get_monthly_date_format(date_string:str) -> pd.Period:
     ----------
     date_string : str
         The date string to parse.
+    kwargs : dict, optional
+        A dictionary of keyword arguments to pass to the function.
         
     Returns
     -------
     pd.Period
         The monthly date format.
     """
-    monthly_date = pd.to_datetime(date_string).to_period('M')
+    monthly_date = pd.to_datetime(date_string, **kwargs).to_period('M')
     return monthly_date
+
+
+def get_econ_predictors(data_path:str = '../../data/econ_predictors_monthly_2021_Amit_Goyal.csv',
+                        START_DATE:str = '1947-01',
+                        END_DATE:str = '2005-04') -> pd.DataFrame:
+    '''
+    This function returns the economic predictions for the given date range.
+
+    Parameters
+    ----------
+    data_path : str
+    START_DATE : str
+    END_DATE : str
+
+    Returns
+    -------
+    pd.DataFrame 
+    '''
+
+    data = pd.read_csv(data_path, index_col=0)
+    data.index = [get_monthly_date_format(str(x), {'format':'%Y%m'}) for x in data.index]
+    econ_data = data[START_DATE:END_DATE]
+
+    equity_price = econ_data['Index'].apply(lambda x: re.sub(r'[^\w\s|.]', '', x))
+    equity_price = equity_price.astype(float)
+    equity_return = equity_price.pct_change()
+    equity_premium = equity_return - econ_data['Rfree']
+    
+    dividend_price_ratio = np.log(econ_data['D12']) - np.log(equity_price)
+
+    dividend_yield = np.log(econ_data['D12']) - np.log(equity_price.shift(1))
+
+    earnings_price_ratio = np.log(econ_data['E12']) - np.log(equity_price)
+
+    earnings_payout_ratio = np.log(econ_data['D12']) - np.log(econ_data['E12'])
+
+    stock_variance = econ_data['svar']
+
+    book_to_market = econ_data['b/m']
+    
+    net_equity_expansion = econ_data['ntis']
+
+    treasury_bill = econ_data['tbl']
+
+    long_term_yield = econ_data['lty']
+
+    long_term_return = econ_data['ltr']
+
+    term_spread = long_term_yield - treasury_bill
+
+    default_yield_spread = econ_data['BAA'] - econ_data['AAA']
+
+    default_return_spread = econ_data['corpr'] - econ_data['ltr']
+
+    inflation = econ_data['infl']
+
+    econ_predictors = pd.concat([equity_premium, 
+                            dividend_price_ratio, 
+                            dividend_yield,
+                            earnings_price_ratio, 
+                            earnings_payout_ratio,
+                            stock_variance,
+                            book_to_market,
+                            net_equity_expansion,
+                            treasury_bill,
+                            long_term_yield,
+                            long_term_return,
+                            term_spread,
+                            default_yield_spread,
+                            default_return_spread, 
+                            inflation], axis=1).dropna()
+                            
+    econ_predictors.columns = ['Equity Premium',
+                            'Dividend Price Ratio', 
+                            'Dividend Yield', 
+                            'Earnings Price Ratio',
+                            'Earnings Payout Ratio',
+                            'Stock Variance',
+                            'Book To Market',
+                            'Net Equity Expansion',
+                            'Treasury Bill',
+                            'Long Term Yield',
+                            'Long Term Return',
+                            'Term Spread',
+                            'Default Yield Spread',
+                            'Default Return Spread',
+                            'Inflation']
+
+    return econ_predictors
